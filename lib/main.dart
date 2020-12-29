1d02527fc41c9f2966686e1da99c923805110a6c
unknown <dnkibere@gmail.com>
2020-12-29 13:45:27 +0300

    before fresh setup lenovo

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:cheki_keja/ui/index.dart';
 import 'package:cheki_keja/ui/introduction.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -12,9 +13,21 @@ void main() async {
 }
 
 class MyApp extends StatelessWidget {
+  bool firsttime =false;
+
+  Future<void> initPrefs() async {
+    var prefs = await SharedPreferences.getInstance();
+    if (!prefs.containsKey('firsttime')) {
+      prefs.setBool('firsttime', false);
+      firsttime = true;
+    } else {
+      firsttime = prefs.getBool('firsttime');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
+    initPrefs();
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
@@ -24,7 +37,7 @@ class MyApp extends StatelessWidget {
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
 
-      home: sharedPreferences.getFirstLogin() ? OnBoardingPage() : Index(),
+      home: firsttime ? OnBoardingPage() : Index(),
     );
   }
 }
